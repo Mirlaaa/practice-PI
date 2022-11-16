4f2c8ee69b3bbdf6185cd0e7b962979ab5b0643b
Mirla Isabel Lopes Gomes <mirla.lopes@academico.ifrn.edu.br>
2022-11-16 16:35:02 -0300

    messages alert

--- estudo_forms/views.py
+++ estudo_forms/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from estudo_forms.forms import AlunoForm
 
 # Create your views here.
 def form_aluno(request):
-    form = AlunoForm
+    form = AlunoForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid:
+            aluno = form.save(commit = False)
+            aluno.save()
+            messages.success(request, 'Cadastro realizado com sucesso!')
+            return redirect('form-aluno')
+    else:
+        form = AlunoForm()
+
     return render(request, 'form.html',{'form':form})
